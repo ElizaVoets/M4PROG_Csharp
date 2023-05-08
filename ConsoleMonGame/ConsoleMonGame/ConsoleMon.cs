ed74904a3c576780c67f3a30aca0f46e9af39c3c
Eliza Voets <25413@ma-web.nl>
2023-05-08 13:11:08 +0200

    day 1 part 1

---
+++ ConsoleMonGame/ConsoleMonGame/ConsoleMon.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleMonGame
+{
+    internal class ConsoleMon
+    {
+        internal int health;
+        internal int energy;
+        internal string name;
+
+        //skill list
+        private List<Skill> skills = new List<Skill>;
+
+        internal void TakeDamage(int damage)
+        {
+            health -= damage;
+        }
+
+        internal void DepleteEnergy(int energyCost)
+        {
+            energy -= energyCost;
+        }
+
+
+    }
+}
---
+++ ListOpdracht/ListOpdracht/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ListOpdracht
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> birds = new List<string> { "Owl", "Crow", "Dog", "Parrot" };
+            List<string> birds2 = new List<string> {"Seagull", "Dove" };
+
+            birds.Remove("Dog");
+            birds.AddRange(birds2);
+
+            foreach(string bird in birds)
+            {
+                Console.WriteLine(bird);
+            }
+        }
+    }
+}
